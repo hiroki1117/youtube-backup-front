19446e5da267b3f51ce94a35fea26d19e2898cde
hiroki <scala1117@gmail.com>
2022-01-31 00:16:48 +0900

    多く表示する機能と検索機能

--- src/Main.tsx
+++ src/Main.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import axios from 'axios';
 import YouTubeIcon from '@mui/icons-material/YouTube';
 import CloudDownloadIcon from '@mui/icons-material/CloudDownload';
+import Button from '@mui/material/Button';
 
 const youtubebackupApiServer = process.env.REACT_APP_API_URL
 
 class Main extends React.Component {
     state = {
-        videos: []
+        videos: [],
+        searchValue: ""
     }
 
     getVideoList = async (uploadStatus: String, fetchNum: Number) => {
@@ -31,6 +33,21 @@ class Main extends React.Component {
         return res
     }
 
+    handleChange = (event: any) => {
+        this.setState({searchValue: event.target.value})
+    }
+
+    handleSubmit = async (event: any) => {
+        const path = "video-info"
+        const res = await axios.get(youtubebackupApiServer + "/" + path ,{
+            params: {
+                video_id: this.state.searchValue
+            }
+        }).then(result => {
+            console.log(result.data)
+            alert(JSON.stringify(result.data))
+        })
+    }
 
     componentDidMount(){
         this.getVideoList("complete", 50)
@@ -41,15 +58,20 @@ class Main extends React.Component {
         
         return (
             <div>
+                <div>
+                    <input type="text" value={this.state.searchValue} onChange={this.handleChange} /><input type="submit" value="検索" onClick={this.handleSubmit}/>
+                </div>
                 <ul>
                     { this.state.videos.map((value) => 
                         <li>
                             <div>
-                                {value["backupdate"]} : <a href={value["video_url"]}><YouTubeIcon /></a> <span onClick={()=> {this.presignedS3(value["video_id"]).then(x => window.open(x.data.presigned_s3url, '_blank'))}}><CloudDownloadIcon /></span> {value["title"]}
+                                {value["backupdate"]} : 
+                                <span onClick={()=> window.open(value["video_url"])}><YouTubeIcon /></span> <span onClick={()=> {this.presignedS3(value["video_id"]).then(x => window.open(x.data.presigned_s3url, '_blank'))}}><CloudDownloadIcon /></span> {value["title"]}
                             </div>
                         </li>    
                     )}
                 </ul>
+                <Button variant="outlined" onClick={()=> this.getVideoList("complete", this.state.videos.length+50)}>more</Button>
             </div>
         )
     }
