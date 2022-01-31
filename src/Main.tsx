181641860fe1cee353ae943a06346f58a9d0db56
hiroki <scala1117@gmail.com>
2022-01-31 13:29:44 +0900

    各種apiリクエストを実装

--- src/Main.tsx
+++ src/Main.tsx
@@ -2,17 +2,18 @@ import React from 'react';
 import axios from 'axios';
 import YouTubeIcon from '@mui/icons-material/YouTube';
 import CloudDownloadIcon from '@mui/icons-material/CloudDownload';
-import Button from '@mui/material/Button';
-import ListGroup from 'react-bootstrap/ListGroup';
-import ListGroupItem from 'react-bootstrap/ListGroupItem';
-import { Container } from 'react-bootstrap';
+// import Button from '@mui/material/Button';
+import { Container, InputGroup, FormControl, ListGroup, Button, DropdownButton, Dropdown} from 'react-bootstrap';
 
 const youtubebackupApiServer = process.env.REACT_APP_API_URL
 
 class Main extends React.Component {
     state = {
         videos: [],
-        searchValue: ""
+        searchValue: "",
+        apiMode: "video-info",
+        apiModeText: "検索",
+        inputValue: ""
     }
 
     getVideoList = async (uploadStatus: String, fetchNum: Number) => {
@@ -36,16 +37,17 @@ class Main extends React.Component {
         return res
     }
 
-    handleChange = (event: any) => {
-        this.setState({searchValue: event.target.value})
+    handleInputChange = (event: any) => {
+        this.setState({inputValue: event.target.value})
     }
 
-    handleSubmit = async (event: any) => {
-        const path = "video-info"
+    handleAPIRequest = async (event: any) => {
+        const path = this.state.apiMode
+
+        const params = path != "videobackup-submit" ? {video_id: this.state.inputValue} : {url: this.state.inputValue}
+
         const res = await axios.get(youtubebackupApiServer + "/" + path ,{
-            params: {
-                video_id: this.state.searchValue
-            }
+            params: params
         }).then(result => {
             console.log(result.data)
             alert(JSON.stringify(result.data))
@@ -62,9 +64,31 @@ class Main extends React.Component {
         return (
             <div>
                 <Container>
-                    <div>
-                        <input type="text" value={this.state.searchValue} onChange={this.handleChange} /><input type="submit" value="検索" onClick={this.handleSubmit}/>
-                    </div>
+                    <InputGroup className="mb-2 mt-3">
+                        <DropdownButton variant="outline-secondary" title={this.state.apiModeText}>
+                            <Dropdown.Item onClick={()=>this.setState({apiMode: "video-info", apiModeText: "検索"})}>
+                                検索
+                            </Dropdown.Item>
+                            <Dropdown.Item onClick={()=>this.setState({apiMode: "videobackup-submit", apiModeText: "保存"})}>
+                                保存
+                            </Dropdown.Item>
+                            <Dropdown.Item onClick={()=>this.setState({apiMode: "video-delete", apiModeText: "削除"})}>
+                                削除
+                            </Dropdown.Item>
+                        </DropdownButton>
+                        <FormControl
+                          placeholder="Recipient's username"
+                          aria-label="Recipient's username"
+                          aria-describedby="basic-addon2"
+                          value={this.state.inputValue}
+                          onChange={this.handleInputChange}
+                        />
+                        <Button id="button-addon2"
+                            onClick={this.handleAPIRequest}
+                        >
+                          リクエスト
+                        </Button>
+                    </InputGroup>
                     <ListGroup>
                         { this.state.videos.map((value) => 
                             <ListGroup.Item action className='m-1'>
@@ -77,8 +101,10 @@ class Main extends React.Component {
                             </ListGroup.Item>    
                         )}
                     </ ListGroup>
-                    <Button variant="outlined" color='info' onClick={()=> this.getVideoList("complete", this.state.videos.length+50)}>more</Button>
-                    </Container>
+                    {this.state.videos.length > 0 &&
+                        <Button variant="light" color='info' onClick={()=> this.getVideoList("complete", this.state.videos.length+50)}>more</Button>
+                    }
+                </Container>
             </div>
         )
     }
